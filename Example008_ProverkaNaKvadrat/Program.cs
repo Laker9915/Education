5084783ee6082459f2700bf7a32c167f2108eb9f
Laker <laker9915@mail.ru>
2022-05-22 16:19:34 +0300

    add_Example008_ProverkaNaKvadrat

---
+++ Example008_ProverkaNaKvadrat/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int check = a * a;
+if (check == b)
+{
+    Console.WriteLine("Верно!");
+}
+else
+{
+    Console.WriteLine("Увы, неверно:(");
+}
