e46b3de038a5e6aa52550886d32eb5a8aea671cb
Laker <laker9915@mail.ru>
2022-05-22 17:05:53 +0300

    add_Example009_Massiv

---
+++ Example009_Massiv/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = -a;
+int c = -a;
+
+while (c < a)
+{
+    Console.WriteLine(c + ", ");
+    c++;
+}
+
+Console.WriteLine(c);
+
