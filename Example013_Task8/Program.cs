2888ad25796bbbf9ba848786009552cbcf611bf0
Laker <laker9915@mail.ru>
2022-05-22 17:58:25 +0300

    add_Example013_Task8

---
+++ Example013_Task8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+
+while (count < n-1)
+{
+    Console.WriteLine(count + ",");
+    count = count + 2;
+}
+
+Console.WriteLine(count);
