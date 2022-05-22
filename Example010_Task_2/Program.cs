9df62c9cb52e56c24d7149059b21e876d175c393
Laker <laker9915@mail.ru>
2022-05-22 17:14:23 +0300

    add_Example010_Task_2

---
+++ Example010_Task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a < b)
+{
+    Console.WriteLine("Первое число меньше второго");
+}
+else
+{
+    Console.WriteLine("Первое число больше второго");
+}
