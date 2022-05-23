4d6d4b0ed1270bb68dd59abc85c9959ce84e506a
laker9915 <laker9915@mail.ru>
2022-05-23 17:11:22 +0500

    lite

--- Seminar3_Task1/Program.cs
+++ Seminar3_Task1/Program.cs
@@ -1,11 +1,7 @@
-﻿Console.WriteLine("Введите координату x первой точки: ");
-double x1 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Введите координату y первой точки: ");
-double y1 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Введите координату x второй точки: ");
-double x2 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Введите координату y второй точки: ");
-double y2 = Convert.ToDouble(Console.ReadLine());
+﻿double x1 = Coordinate("x", "A");
+double x2 = Coordinate("y", "A");
+double y1 = Coordinate("x", "B");
+double y2 = Coordinate("y", "B");
 
 double katet1 = (x2 - x1) * (x2 - x1);
 double katet2 = (y2 - y1) * (y2 - y1);
@@ -17,3 +13,9 @@ double Gipotenusa(double sum)
     return Math.Sqrt(sum);
 }
 Console.WriteLine($"Расстояние между точками равно {distance}");
+
+double Coordinate(string coorName, string pointName)
+{
+    Console.Write($"Введите координату {coorName} точки {pointName}: ");
+    return Convert.ToDouble(Console.ReadLine());
+}
