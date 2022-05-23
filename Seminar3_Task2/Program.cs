a1fa897f58f9107fadff78d687b80d209d5ad5b4
laker9915 <laker9915@mail.ru>
2022-05-23 17:22:57 +0500

    add_Seminar3_Task2

---
+++ Seminar3_Task2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+
+while (index <= N)
+{
+    int square = index*index;
+    Console.Write($"{square} ");
+    index++;
+}
