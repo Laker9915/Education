4d7969fdf857d37c4e4021963442c64520b9c2c6
laker9915 <laker9915@mail.ru>
2022-05-23 13:28:51 +0500

    add_Seminar2_Task1

---
+++ Seminar2_Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+
+int DeleteSecond(int number)
+{
+    int first = number / 100;
+    int second = number % 10;
+    int result = first * 10 + second;
+    return result;
+}
+
+int result = DeleteSecond(number);
+Console.WriteLine(result);
