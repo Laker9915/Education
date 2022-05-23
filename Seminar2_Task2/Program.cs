29d8152375cf414cecfb11564590cfffeed64a13
laker9915 <laker9915@mail.ru>
2022-05-23 14:24:35 +0500

    add_Seminar2_Task2

---
+++ Seminar2_Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (ostatok(numberA, numberB) != 0)
+{
+    Console.Write("Остаток: ");
+    Console.WriteLine(ostatok(numberA, numberB));
+}
+
+else 
+{
+    Console.Write("Ответ: ");
+    Console.WriteLine(numberA / numberB);
+}
+int ostatok(int numberA, int numberB)
+{
+    return numberA % numberB;
+} 
