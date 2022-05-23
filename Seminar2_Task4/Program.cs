4a1f4a9f3765133c3ccd73ffa8909d8b9787057a
laker9915 <laker9915@mail.ru>
2022-05-23 15:27:48 +0500

    add_Seminar2_Task4

---
+++ Seminar2_Task4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99)
+{
+    string array = Convert.ToString(number);
+    Console.WriteLine(array[2]);
+}
+else
+    Console.WriteLine("Третьей цифры нет!");
